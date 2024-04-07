17fe977ea2b95df5985f38306412ed07b4fe36cc
JulianP152002 <parracardenas17@gmail.com>
2024-04-07 11:41:30 -0500

    mapeo de cars

---
+++ src/const/carsm.ts
@@ -0,0 +1,100 @@
+export const CARSM = [
+    {
+        id: "M3COMPETITON",
+        name: "BMW COMPETITION 2024",
+        fuel: "Gasolina",
+        precie: "150'000.000",
+    },
+    {
+        id: "M440iCoupe",
+        name: "BMW M440i Coupe 2024",
+        fuel: "Gasolina",
+        precie: "180'000.000",
+    },
+    {
+        id: "X4M40i2024",
+        name: "BMW X4 M40i 2024",
+        fuel: "Gasolina",
+        precie: "150'000.000",
+    },
+    {
+        id: "XM202",
+        name: "BMW XM 2024",
+        fuel: "Gasolina",
+        precie: "150'000.000",
+    },
+    {
+        id: "Z4M40i202",
+        name: "BMW Z4 M40i 2024",
+        fuel: "Gasolina",
+        precie: "150'000.000",
+    },
+    {
+        id: "M440ixDriveGranCoupé",
+        name: "BMW M440i xDrive Gran Coupé",
+        fuel: "Gasolina",
+        precie: "205000000"
+    },
+    {
+        id: "BMWX5",
+        name: "BMW X5 2024",
+        fuel: "Gasolina",
+        precie: "205000000"
+    },
+    {
+        id: "BMWX6M",
+        name: "BMW X6 M 2024",
+        fuel: "Gasolina",
+        precie: "205000000"
+    },
+    {
+        id: "BMWX3",
+        name: "BMW X3 2024",
+        fuel: "Gasolina",
+        precie: "xx"
+    },
+    {
+        id: "BMWiX2",
+        name: "BMW iX2",
+        fuel: "Electrico",
+        precie: "xx"
+    },
+
+    {
+        id: "BMWSerie5",
+        name: "BMW Serie 5 Sedán 2023",
+        fuel: "Electrico",
+        precie: "xx"
+    },
+    {
+        id: "M135ixDrive",
+        name: "BMW M135i xDrive",
+        fuel: "Electrico",
+        precie: "xx"
+    },
+    {
+        id: "X2M35ixDrive",
+        name: "BMW X2 M35i xDrive",
+        fuel: "Electrico",
+        precie: "xx"
+    },
+    {
+        id: "BMWi4",
+        name: "BMW i4",
+        fuel: "Electrico",
+        precie: "xx"
+    },
+    {
+        id: "M2Coupé",
+        name: "BMW M2 Coupé",
+        fuel: "Electrico",
+        precie: "xx"
+    },
+    {
+        id: "Serie4Cabrio",
+        name: "BMW M440i Convertible",
+        fuel: "Gasolina",
+        precie: "xx"
+    },
+
+];
